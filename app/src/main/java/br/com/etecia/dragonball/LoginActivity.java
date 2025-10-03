12438ac6de50c968c72939f4ef6514fa70cbeffa
lcssoaress <lcssoaresmachado@gmail.com>
2025-10-02 21:28:35 -0300

    primeiro commit

--- app/src/main/java/br/com/etecia/dragonball/LoginActivity.java
+++ app/src/main/java/br/com/etecia/dragonball/LoginActivity.java
@@ -1,6 +1,7 @@
 package br.com.etecia.dragonball;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +9,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class LoginActivity extends AppCompatActivity {
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
 
+public class LoginActivity extends AppCompatActivity {
+TextInputEditText txtemail,txtsenha;
+MaterialButton btnlogin,btnsign;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +25,18 @@ public class LoginActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        txtemail = findViewById(R.id.txtemail);
+        txtsenha = findViewById(R.id.txtsenha);
+        btnlogin = findViewById(R.id.btnlogin);
+
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email, senha;
+                email = txtemail.getText().toString()
+                senha = txtsenha.getText().toString();
+            }
+        });
     }
+
 }
