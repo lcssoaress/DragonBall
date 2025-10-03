5bfda05518c8705d6c5b2a910fc66d3ead43fcbd
lcssoaress <lcssoaresmachado@gmail.com>
2025-10-02 22:13:01 -0300

    primeiro commit

--- app/src/main/java/br/com/etecia/dragonball/SignInActivity.java
+++ app/src/main/java/br/com/etecia/dragonball/SignInActivity.java
@@ -8,26 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class CardMineActivity extends AppCompatActivity {
+public class SignInActivity extends AppCompatActivity {
 
-    menu
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.card_mine_layout);
+        setContentView(R.layout.sign_in_layout);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        iLogOut = findViewById(R.id.iLogOut);
-
-        iLogOut.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
-                finish();
-            }
     }
 }
